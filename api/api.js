9aacddf914b4298d541311e85b0f2f3023abd2c9
Ulises Layera <ulayera@externos.bancoestado.cl>
2018-07-12 14:20:18 -0400

    tareas: mock servicio, cliente servicio

--- api/api.js
+++ api/api.js
@@ -1,6 +1,7 @@
 const { router, get, post } = require('microrouter');
 const { getCuentasInscritas } = require('../repository/getCuentasInscritas/getCuentasInscritas.controller');
 const { getDetalleCuenta } = require('../repository/getDetalleCuenta/getDetalleCuenta.controller');
+const { realizarPagoCuenta } = require('../repository/realizarPagoCuenta/realizarPagoCuenta.controller');
 const { healthcheck } = require('../repository/healthcheck/healthcheck');
 const { notfound } = require('../repository/notFound/notFound');
 
@@ -8,6 +9,7 @@ const api = router(
   get('/microservicio/v1/pagoservicios/personasnaturales/healthcheck', healthcheck),
   get('/microservicio/v1/pagoservicios/cuentas-inscritas/:rut', getCuentasInscritas),
   get('/microservicio/v1/pagoservicios/detalle-cuenta/:identificacion/:conceptoPago', getDetalleCuenta),
+  post('/microservicio/v1/pagoservicios/realizar-pago-cuenta', realizarPagoCuenta),
   get('/*', notfound),
   post('/*', notfound),
 );
